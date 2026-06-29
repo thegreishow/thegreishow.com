56fe0c76ec65321fc7d1a7e3cd23b00d735057a7
thegreishow <thegreishow@gmail.com>
2026-06-29 17:22:45 -0500

    Update video and documentary entries in media engine

--- assets/js/media-engine.js
+++ assets/js/media-engine.js
@@ -7,17 +7,17 @@ window.MediaEngine = (function () {
 
   const state = {
     videos: [
-      { title: "No Drama", id: "ZsoVettmghI", type: "release", artist: "The Grei Show" },
-      { title: "Video 2", id: "aGXBlYq3EbY", type: "release", artist: "The Grei Show" },
-      { title: "Video 3", id: "Ibd1_8niIhg", type: "release", artist: "The Grei Show" },
-      { title: "Video 4", id: "xTqZHV_37Lo", type: "release", artist: "The Grei Show" },
-      { title: "Video 5", id: "jinbfL-dW7s", type: "release", artist: "The Grei Show" },
-      { title: "Video 6", id: "8HcD4kv-oAI", type: "release", artist: "The Grei Show" },
-      { title: "Video 7", id: "dGydgjCmkb4", type: "release", artist: "The Grei Show" }
+      { title: "Puff Puff Pass [Remix]", id: "ZsoVettmghI", type: "release", artist: "The Grei Show, Bay-C" },
+      { title: "Puff Puff Pass", id: "aGXBlYq3EbY", type: "release", artist: "The Grei Show" },
+      { title: "Choppa Talk", id: "Ibd1_8niIhg", type: "release", artist: "The Grei Show" },
+      { title: "Loosen Up", id: "xTqZHV_37Lo", type: "release", artist: "The Grei Show, Swnkah" },
+      { title: "The Vibe", id: "jinbfL-dW7s", type: "release", artist: "The Grei Show, Big Sugar, Canned Beets" },
+      { title: "Joy", id: "8HcD4kv-oAI", type: "release", artist: "The Grei Show" },
+      { title: "Blind Without Shades", id: "dGydgjCmkb4", type: "release", artist: "The Grei Show" }
     ],
 
     documentaries: [
-      { title: "Behind The Grei Show", id: "iRzHXezaJ_E", type: "doc" }
+      { title: "Exploring Local Dispensaries In Kingston With The Legandary Bay-C", id: "iRzHXezaJ_E", type: "doc" }
     ],
 
     liveSessions: [

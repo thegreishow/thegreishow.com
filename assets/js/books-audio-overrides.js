ae1e798723e30a7c64326a04dac7169ea7d11385
thegreishow <thegreishow@gmail.com>
2026-07-15 14:59:41 -0500

    Refresh opening Books narration links

---
+++ assets/js/books-audio-overrides.js
@@ -0,0 +1,18 @@
+if (typeof chapters !== 'undefined') {
+  Object.assign(chapters[0], {
+    previewAudio: 'https://storage.googleapis.com/adm--audio-playback--7d--public/mcp-preview/eeaa8a51-7762-48a2-b303-a40bf6397e26.mp3',
+    fullAudio: 'https://www.aidocmaker.com/g0/audio?name=d0efd06c2ad34f28bd312716bf86d24e'
+  });
+
+  Object.assign(chapters[1], {
+    previewAudio: 'https://storage.googleapis.com/adm--audio-playback--7d--public/mcp-preview/94d9b2d1-fe2a-4f92-84b6-14a2e0026cd0.mp3',
+    fullAudio: 'https://www.aidocmaker.com/g0/audio?name=66e5dbf958cc45e38a74b9271d60df44'
+  });
+
+  Object.assign(chapters[2], {
+    previewAudio: 'https://storage.googleapis.com/adm--audio-playback--7d--public/mcp-preview/45a65a27-b636-4dde-90c5-eeeb54d53c6a.mp3',
+    fullAudio: 'https://www.aidocmaker.com/g0/audio?name=78bb3f3f90934dd588b0149ec2a8ea06'
+  });
+
+  renderChapter();
+}

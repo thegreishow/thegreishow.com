0999bd46d8143784e8cd1a2e99423417c9e4142f
thegreishow <thegreishow@gmail.com>
2026-07-19 16:22:38 -0500

    Add configurable White Line payment providers

---
+++ assets/js/whiteline-payments-config.js
@@ -0,0 +1,10 @@
+window.WHITE_LINE_PAYMENTS = {
+  // Add the public payment/profile URLs clients should open. Leave blank until confirmed.
+  paypalUrl: '',
+  wiseUrl: '',
+  payoneerUrl: '',
+
+  // Keep sensitive account numbers out of this public file. Use general transfer instructions only.
+  bankInstructions: 'Reply to this message to receive the appropriate bank-transfer details.',
+  confirmationEmail: 'thegreishow@gmail.com'
+};

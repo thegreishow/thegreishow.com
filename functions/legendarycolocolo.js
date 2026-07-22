223a7aef67b82d206539203e9ba59e5c1794f66e
thegreishow <thegreishow@gmail.com>
2026-07-22 02:04:58 -0500

    Serve Colo Colo from resilient static profile

--- functions/legendarycolocolo.js
+++ functions/legendarycolocolo.js
@@ -1,5 +1,5 @@
 export async function onRequest({ request, env }) {
-  const profileUrl = new URL('/whiteline-profile.html?slug=legendarycolocolo', request.url);
+  const profileUrl = new URL('/legendarycolocolo.html', request.url);
   const profileRequest = new Request(profileUrl.toString(), {
     method: request.method === 'HEAD' ? 'HEAD' : 'GET',
     headers: request.headers

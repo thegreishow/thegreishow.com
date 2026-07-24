e47bb4ed94bb607dcdc320b02c5d47b10c82e9eb
thegreishow <thegreishow@gmail.com>
2026-07-24 16:29:06 -0500

    Integrate CMS into Owner OS and hydrate CMS release fields

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,6 +3,9 @@ export async function onRequest(context){
   const type=response.headers.get('content-type')||'';
   if(!type.includes('text/html'))return response;
   const path=new URL(context.request.url).pathname;
-  if(path.startsWith('/owner-cms')||path.startsWith('/whiteline-admin'))return response;
-  return new HTMLRewriter().on('body',{element(el){el.append('<script src="/assets/js/cms-public.js" defer></script>',{html:true})}}).transform(response);
+  if(path.startsWith('/whiteline-admin'))return response;
+  return new HTMLRewriter().on('body',{element(el){
+    if(path.startsWith('/owner'))el.append('<script src="/assets/js/owner-cms-enhancements.js" defer></script>',{html:true});
+    else el.append('<script src="/assets/js/cms-public.js" defer></script>',{html:true});
+  }}).transform(response);
 }

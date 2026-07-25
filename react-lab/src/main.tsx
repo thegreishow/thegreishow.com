83afbfdcfa4002c0932ab2bf78119c308b0025c8
thegreishow <thegreishow@gmail.com>
2026-07-25 13:42:57 -0500

    Remove obsolete experimental design styles

--- react-lab/src/main.tsx
+++ react-lab/src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
-import './arcade-preview.css';
-import './design-v3.css';
 
 const root = document.getElementById('root');
 

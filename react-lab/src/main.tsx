fc7eb696486b6577317ca02fdf3910f0a9de5f02
thegreishow <thegreishow@gmail.com>
2026-07-24 19:59:47 -0500

    Load React arcade showcase styles

--- react-lab/src/main.tsx
+++ react-lab/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './arcade-preview.css';
 
 const root = document.getElementById('root');
 

76dba057c59a754d5eb4492cd7d0f44afde64c05
thegreishow <thegreishow@gmail.com>
2026-07-25 13:33:09 -0500

    Load cinematic design layer

--- react-lab/src/main.tsx
+++ react-lab/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './arcade-preview.css';
+import './design-v3.css';
 
 const root = document.getElementById('root');
 

d18bc25748315284ce25d65c5ca7fceac3c1775f
thegreishow <thegreishow@gmail.com>
2026-07-26 21:37:22 -0500

    Load streamlined music and promo navigation scripts

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -7,5 +7,7 @@ export async function onRequest(context){
   return new HTMLRewriter().on('body',{element(el){
     if(path.startsWith('/owner'))el.append('<script src="/assets/js/owner-cms-enhancements.js" defer></script>',{html:true});
     else el.append('<script src="/assets/js/cms-public.js" defer></script>',{html:true});
+    if(path==='/music'||path==='/music/'||path==='/music.html')el.append('<script src="/assets/js/music-promo-links.js" defer></script>',{html:true});
+    if(path==='/promo'||path.startsWith('/promo/'))el.append('<script src="/assets/js/promo-navigation.js" defer></script>',{html:true});
   }}).transform(response);
 }
